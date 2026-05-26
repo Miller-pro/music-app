f12d9693119fa9a564edf2586b9e70d904bdff7c
Miller-pro <dean@m-innovation-group.com>
2026-05-26 15:46:22 +0300

    Remove CSP headers causing build failure

--- audioverse-publishers/next.config.mjs
+++ audioverse-publishers/next.config.mjs
@@ -2,18 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
-          }
-        ]
-      }
-    ];
-  }
 };
 export default nextConfig;
